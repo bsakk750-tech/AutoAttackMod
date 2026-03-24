f33ab8cbfcc88f9c91663e0b80095e2f79d673c1
bsakk750-tech <bsakk750@gmail.com>
2026-03-24 13:05:46 +0300

    Create AutoAttackMod.java file

---
+++ src/main/java/com/bsakk750/autoattack/AutoAttackMod.java
@@ -0,0 +1,15 @@
+package com.bsakk750.autoattack;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoAttackMod implements ModInitializer {
+    public static final String MOD_ID = "autoattack";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Auto Attack Mod initialized!");
+    }
+}
